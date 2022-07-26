a87cf7c0a9ca20a9ae73e3f8599d494e519848b2
kdadika <kdadika@gmail.com>
2022-07-25 21:39:48 -0400

    Completed Training JS #1 8kyu

---
+++ 8kyu/trainingJs.js
@@ -0,0 +1,22 @@
+// In JavaScript, your code is running in a function, let us step by step complete your first JS function.
+
+// Look at this example:
+
+//       --------keyword "function"
+//       |       ----your function name  
+//       |       |    ---if needed, some arguments will appear in brackets
+//       |       |    |
+//     function myfunc(){  ---------your function code will starting with "{"
+//       //you should type your code here
+//     }----------------------------ending with "}"
+// If we want to print some to the screen, maybe we can use Document.write() in the web, or use alert() pop your message, but Codewars did not support these methods, we should use console.log() in your function. see this example:
+
+// function printWordToScreen(){
+//   var somewords="This is an example."
+//   console.log(somewords)
+// }
+
+function helloWorld() {
+	let str = "Hello World!"
+	console.log(str)
+  }
