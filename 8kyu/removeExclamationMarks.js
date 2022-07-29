2b4afb1c73ec178ba48e492741e05a208c6bfd55
kdadika <kdadika@gmail.com>
2022-07-28 22:08:52 -0400

    Completed Remove Exclamation Marks 8kyu

---
+++ 8kyu/removeExclamationMarks.js
@@ -0,0 +1,11 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+	return s.split('!').join('')
+  }
+
+
+// SUGGESTED ANSWER
+//   function removeExclamationMarks(s) {
+// 	return s.replace(/!/gi, '');
+//   }
